c525104a0dc788d7272def389109cdbc04ef02a4
Colombo, Edoardo <Edoardo.Colombo@wolterskluwer.com>
2026-03-21 23:25:25 +0100

    feat: added global spinner

--- apps/angular/5-crud-application/src/app/app.component.ts
+++ apps/angular/5-crud-application/src/app/app.component.ts
@@ -1,11 +1,18 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
 import { Todo } from './models/crud.model';
 import { CrudService } from './services/crud.service';
+import { LoadingService } from './services/loading.service';
 
 @Component({
-  imports: [],
+  imports: [MatProgressSpinner],
   selector: 'app-root',
   template: `
+    @if (loadingService.loading()) {
+      <div class="spinner-overlay">
+        <mat-spinner />
+      </div>
+    }
     @for (todo of todos(); track todo.id) {
       {{ todo.id }}
       {{ todo.title }}
@@ -14,9 +21,25 @@ import { CrudService } from './services/crud.service';
       <br />
     }
   `,
-  styles: [],
+  styles: [
+    `
+      .spinner-overlay {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background: rgba(0, 0, 0, 0.3);
+        z-index: 1000;
+      }
+    `,
+  ],
 })
 export class AppComponent implements OnInit {
+  loadingService = inject(LoadingService);
   todos = signal<Todo[]>([]);
 
   constructor(private crudService: CrudService) {}
--- apps/angular/5-crud-application/src/app/app.config.ts
+++ apps/angular/5-crud-application/src/app/app.config.ts
@@ -2,10 +2,11 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { errorInterceptor } from './interceptors/error.interceptor';
+import { loadingInterceptor } from './interceptors/loading.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(withInterceptors([errorInterceptor])),
+    provideHttpClient(withInterceptors([errorInterceptor, loadingInterceptor])),
     provideAnimations(),
   ],
 };
